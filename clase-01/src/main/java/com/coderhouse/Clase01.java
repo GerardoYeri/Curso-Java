23eca5ada34687e39662e7833e9029e28b8a6dd1
Geri <Geri@192.168.5.100>
2024-11-16 01:39:42 -0300

    "Clase01 completa"

--- clase-01/src/main/java/com/coderhouse/Clase01.java
+++ clase-01/src/main/java/com/coderhouse/Clase01.java
@@ -25,6 +25,65 @@ public class Clase01 {
 		
 		String cadenaDeTexto = "Mi primer string";
 		
+		System.out.println(cadenaDeTexto);
+		
+		System.out.println(caracter + cadenaDeTexto);
+		
+		System.out.println(caracter + " " + numeroCorto);
+		
+		int numeroUno = 10;
+		int numeroDos = 20;
+		
+		int resultado = numeroUno + numeroDos;
+		
+		System.out.println("la suma es: "+resultado);
+		
+		
+		
+		resultado = numeroUno - numeroDos;
+		
+		System.out.println("la resta es: "+resultado);
+		
+		resultado = numeroUno % numeroDos;
+		
+		System.out.println("el RESTO es: "+resultado);
+		
+		//Incrementar
+		
+		int contador = 0;
+		System.out.println(contador);
+		contador = 1; //reasignar el valor
+		System.out.println(contador);
+		contador = contador; // se reasigna a si mismos el valor que tenia antes
+		System.out.println(contador);
+		contador = contador + 1; //incrementar el valor
+		System.out.println(contador);
+		contador++; //incrementa el valor
+		System.out.println(contador);
+		contador = contador -1; //decrementa el valor
+		System.out.println(contador);
+		contador--; //decrementa su valor
+		
+		//operadores
+		
+		int numA= 20;
+		int numB= 30;
+		
+		System.out.println(numA == numB); //son iguales?
+		System.out.println(numA != numB); //son distintos?
+		
+		System.out.println(numA < numB); // numA es MENOR a numB?
+		System.out.println(numA > numB); // numA es MAYOR a numB?
+		
+		System.out.println(numA <= numB); // numA es MENOR IGUAL a numB
+		System.out.println(numA >= numB); // numA es MAYOT IGUAL a numB
+		
+		final int NUMERO_CONSTANTE = 1000;
+		
+		System.out.println(NUMERO_CONSTANTE);
+		
+		
+		
 		
 
 	}
