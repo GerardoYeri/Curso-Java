8c06082d4d6db1882505cab9a2b04e3107663c23
Geri <Geri@192.168.5.100>
2024-11-16 00:55:13 -0300

    "ejemplos clase 01"

---
+++ clase-01/src/main/java/com/coderhouse/Clase01.java
@@ -0,0 +1,32 @@
+package com.coderhouse;
+
+public class Clase01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola coders!!!");
+		
+		//Declaracion de variables
+		
+		byte numeroChico = 100;
+		
+		int numeroGrade = 123225;
+		
+		char caracter = 'G';
+		
+		double numeroConDecimal = 123.43d;
+		
+		float numeroConDecimaFloat = 1434.121f;
+		
+		long numeroMuyLargo = 423252453;
+		
+		short numeroCorto = 1343;
+		
+		boolean verdadero = false;
+		
+		String cadenaDeTexto = "Mi primer string";
+		
+		
+
+	}
+
+}
