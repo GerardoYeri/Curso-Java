a69451dd9f0aba4342905ed48aaa370726fb850e
Geri <Geri@192.168.5.101>
2024-12-08 21:54:17 -0300

    ejercicios clase 5

---
+++ clase05/src/main/java/com/coderhouse/ejercicios/objetos/Empleado.java
@@ -0,0 +1,16 @@
+//5.Crea una clase Empleado con un atributo sueldo y una subclase Gerente que tenga un método para calcular bonificaciones
+package com.coderhouse.ejercicios.objetos;
+
+public class Empleado {
+		protected double sueldo;
+
+		public double getSueldo() {
+			return sueldo;
+		}
+
+		public void setSueldo(double sueldo) {
+			this.sueldo = sueldo;
+		}
+		
+		
+}
